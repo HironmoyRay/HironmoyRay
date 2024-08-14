203c33bb20bb9123cfde0d22c50e3dd42f4492c6
hironmoy-ray <hpray35@gmail.com>
2024-08-14 20:08:30 +0600

    Info Update: Experience Section

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var typed = new Typed (".typing", {
-    strings:["Junior Developer","MERN Learner", "Problem-Solving Enthusiast","Frontend Developer","DSA Explorer"],
+    strings:["Web Developer","MERN Learner", "Problem-Solving Enthusiast","Frontend Developer","DSA Explorer"],
     typeSpeed:120,
     BackSpeed:20,
     loop:true
